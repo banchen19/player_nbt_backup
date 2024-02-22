546235e1639faab12d3ef60333209077cbab7ad5
banchen19 <banchen19@outlook.com>
2024-02-23 03:19:14 +0800

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# GMLIB-Plugin-Template
+# GMLIB-Plugin-Template——玩家nbt数据备份
 Template for plugins using GMLIB
+
+
+本示例同时使用了ll与gmlib的api。
+
+本项目代码所有权归属于 PHEyeji（QQ号：1102581553）
+
+提供了非常详细的注释
+--
+玩家进入游戏时创建数据文件，位于data文件夹中，
+当删除data文件夹时候，会重新创建玩家数据文件
+如果玩家数据文件存在，则会读取玩家的nbt数据，写入给玩家，由于为了方便操作玩家nbt，写入时用是snbt，如果你有需求，可以找到玩家的数据文件进行修改，当然这样并不安全。但是提供了这样一种操作。
+
+---
+感谢 来自PHEyeji（QQ号：1102581553）的定制，以及IKUN大佬（项目来源：https://github.com/GroupMountain ）的技术帮助，
+
--- src/Entry.cpp
+++ src/Entry.cpp
@@ -6,6 +6,15 @@
 #include <ll/api/io/FileUtils.h>
 #include <ll/api/plugin/NativePlugin.h>
 #include <ll/api/plugin/PluginManagerRegistry.h>
+
+
+#include <direct.h>
+#include <fstream>
+#include <io.h>
+#include <iostream>
+#include <string>
+
+
 #include <memory>
 #include <stdexcept>
 ll::Logger logger(PLUGIN_NAME);
@@ -14,35 +23,168 @@ namespace change_this {
 
 namespace {
 
+
+using namespace std;
+// ll3 事件注册，玩家进入游戏
+ll::event::ListenerPtr playerJoinEventListener;
+// ll3 事件注册，玩家退出游戏
+ll::event::ListenerPtr player_life_game_Listener;
+// ll3 事件注册，玩家重生事件
+ll::event::ListenerPtr player_respawn_Listener;
+// ll3 事件注册，玩家跳跃事件
+ll::event::ListenerPtr player_jump_Listener;
 std::unique_ptr<std::reference_wrapper<ll::plugin::NativePlugin>>
     selfPluginInstance; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
 
 auto disable(ll::plugin::NativePlugin& /*self*/) -> bool {
     logger.info("disabling...");
 
-    // Your code here.
+    auto& eventBus = ll::event::EventBus::getInstance();
+    // 取消事件：玩家进入游戏
+    eventBus.removeListener(playerJoinEventListener);
+    // 取消事件：玩家退出游戏
+    eventBus.removeListener(player_life_game_Listener);
+    // 取消事件：玩家重生事件
+    eventBus.removeListener(player_respawn_Listener);
+    // 取消事件：玩家跳跃事件
+    eventBus.removeListener(player_jump_Listener);
 
     logger.info("disabled");
-
     return true;
 }
 
-auto enable(ll::plugin::NativePlugin& /*self*/) -> bool {
+// 检测文件是否存在
+bool isFileExists_ifstream(string& name) {
+    ifstream f(name.c_str());
+    return f.good();
+}
+
+// 读取文件内的nbt给玩家
+bool read_fileto_playernbt(mce::UUID uuid, string player_path) {
+
+    // 删除玩家的nbt
+    GMLIB_Player::deletePlayerNbt(uuid);
+
+    ifstream infile;
+    // 打开文件player_path
+    ifstream fin(player_path);
+    // stringstream object
+    stringstream buffer;
+    // read file content in stringstream object
+    buffer << fin.rdbuf();
+    // store file content in a string
+    string str(buffer.str());
+    // 使用CompoundTag::fromSnbt将data转换为CompoundTag
+    // logger.info("玩家snbt:\n{}", str);
+    auto file_toplayer_nbt = CompoundTag::fromSnbt(str);
+    // 所以这里的第二个参数需要*来解引用
+    auto player_write = GMLIB_Player::setPlayerNbt(uuid, *file_toplayer_nbt);
+    // 检查是写入到玩家nbt成功
+    return player_write;
+}
+
+
+// 将玩家的nbt写入到文件
+bool player_write_nbt(mce::UUID uuid, string player_path) {
+    auto player_nbt = GMLIB_Player::getPlayerNbt(uuid);
+    // 将snbt转换为snbt
+    const auto nbt_string = player_nbt->toSnbt();
+    // 输出到文件player_path，没有就创建
+    ofstream outfile(player_path);
+    // 输入nbt_string
+    outfile << nbt_string;
+    // logger.info("玩家snbt:\n{}", nbt_string);
+    // 关闭文件
+    outfile.close();
+    return true;
+}
+
+auto enable(ll::plugin::NativePlugin& self) -> bool {
+    auto& logger = getSelfPluginInstance().getLogger();
     logger.info("enabling...");
 
-    logger.info("enabled");
+    auto& eventBus = ll::event::EventBus::getInstance();
 
+    // 注册玩家登录事件监听
+    playerJoinEventListener = eventBus.emplaceListener<GMLIB::Event::PlayerEvent::PlayerLoginAfterEvent>(
+        [&logger, &self](GMLIB::Event::PlayerEvent::PlayerLoginAfterEvent& event) {
+            // 从SDK-GMLIB\GMLIB\Event\Player\PlayerLoginEvent.h 拿到uuid  这里拿到的uuid是mce::uuid
+            auto   uuid         = event.getUuid();
+            auto   player_name  = event.getRealName();
+            string dataFilePath = self.getDataDir().string();
+
+            // 将uuid字符串化
+            string player_path = dataFilePath + "/" + uuid.asString() + ".json";
+            // 查询存储玩家的snbt文件是否存在
+            bool ret = isFileExists_ifstream(player_path);
+            if (ret) {
+                logger.info("正在读取玩家{}->数据文件： {}", player_name, player_path);
+                auto player_write = read_fileto_playernbt(uuid, player_path);
+
+                if (player_write) {
+                    logger.info("读取成功->{}", player_name);
+                } else {
+                    logger.info("读取失败->{}", player_name);
+                }
+            } else {
+                logger.info("正在初始化玩家{}-> 数据文件： {}", player_name, player_path);
+                // 通过离线api获取玩家CompoundTag
+                player_write_nbt(uuid, player_path);
+            }
+        }
+    );
+
+    // 注册玩家退出游戏监听
+    player_life_game_Listener = eventBus.emplaceListener<ll::event::player::PlayerLeaveEvent>(
+        [&logger, &self](ll::event::player::PlayerLeaveEvent& event) {
+            // 从SDK-GMLIB\GMLIB\Event\Player\PlayerLoginEvent.h 拿到uuid  这里拿到的uuid是mce::uuid
+            auto&  player       = event.self();
+            auto   uuid         = player.getUuid();
+            auto   player_name  = player.getRealName();
+            string dataFilePath = self.getDataDir().string();
+            string player_path  = dataFilePath + "/" + uuid.asString() + ".json";
+            player_write_nbt(uuid, player_path);
+        }
+    );
+    // 玩家重生
+    player_respawn_Listener = eventBus.emplaceListener<ll::event::player::PlayerRespawnEvent>(
+        [&logger, &self](ll::event::player::PlayerRespawnEvent& event) {
+            // 从SDK-GMLIB\GMLIB\Event\Player\PlayerLoginEvent.h 拿到uuid  这里拿到的uuid是mce::uuid
+            auto&  player       = event.self();
+            auto   uuid         = player.getUuid();
+            auto   player_name  = player.getRealName();
+            string dataFilePath = self.getDataDir().string();
+            string player_path  = dataFilePath + "/" + uuid.asString() + ".json";
+            player_write_nbt(uuid, player_path);
+        }
+    );
+
+    // ll3 事件注册，玩家跳跃事件
+    player_jump_Listener = eventBus.emplaceListener<ll::event::player::PlayerJumpEvent>(
+        [&logger, &self](ll::event::player::PlayerJumpEvent& event) {
+            // 从SDK-GMLIB\GMLIB\Event\Player\PlayerLoginEvent.h 拿到uuid  这里拿到的uuid是mce::uuid
+            auto&  player       = event.self();
+            auto   uuid         = player.getUuid();
+            auto   player_name  = player.getRealName();
+            string dataFilePath = self.getDataDir().string();
+            string player_path  = dataFilePath + "/" + uuid.asString() + ".json";
+            player_write_nbt(uuid, player_path);
+        }
+    );
+    logger.info("enabled");
     return true;
 }
 
+
 auto load(ll::plugin::NativePlugin& self) -> bool {
-    logger.info("loading...");
+    logger.info("玩家nbt数据备份加载中");
 
     selfPluginInstance = std::make_unique<std::reference_wrapper<ll::plugin::NativePlugin>>(self);
 
-    // Your code here.
-
-    logger.info("loaded");
+    string plugin_name  = (PLUGIN_NAME);
+    string datafile_dir = self.getDataDir().string();
+    int    i            = mkdir(datafile_dir.c_str());
+    logger.info("玩家nbt数据备份加载完毕");
 
     return true;
 }
